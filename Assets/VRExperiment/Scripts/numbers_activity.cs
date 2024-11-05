026816e5ffa1c5be0c1ee6afdc41e0d1b542ed8e
Marcos Hernández <126048022+marcosnigp1@users.noreply.github.com>
2024-11-05 13:59:37 +0400

    Testing script added

--- Assets/VRExperiment/Scripts/numbers_activity.cs
+++ Assets/VRExperiment/Scripts/numbers_activity.cs
@@ -7,9 +7,22 @@ using TMPro; //This will help in getting the UI values.
 public class numbers_activity : MonoBehaviour
 {
 
-    public TextMeshProUGUI output; //Add field for TextMeshProUGUI
+    public TextMeshProUGUI main_output; //Main testing field
+
+    //Texting output fields
+    public TextMeshProUGUI output1;
+    public TextMeshProUGUI output2; 
+    public TextMeshProUGUI output3;
+    public TextMeshProUGUI output4;
 
     public void ChangeValue(){
-        output.text = "Hello world!";
+        main_output.text = "Hello world!";
+
+
+        //Show values.
+        output1.text = "Hi!";
+        output2.text = "Hi!";
+        output3.text = "Hi!";
+        output4.text = "Hi!";
     }
 }
